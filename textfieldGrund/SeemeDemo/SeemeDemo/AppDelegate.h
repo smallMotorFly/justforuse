9ca78ea99ef19682c4752aab5fcf5e60d6e82341
zhangbin <zhangbin@handlecar.com>
2017-01-06 23:34:36 +0800

    添加空白工程
    添加空白工程至git仓库

---
+++ textfieldGrund/SeemeDemo/SeemeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeemeDemo
+//
+//  Created by Handlecar1 on 17/1/6.
+//  Copyright © 2017年 Handlecar1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textfieldGrund/SeemeDemo/SeemeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeemeDemo
+//
+//  Created by Handlecar1 on 17/1/6.
+//  Copyright © 2017年 Handlecar1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
